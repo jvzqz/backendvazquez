5a2df6ab1f9ecc6c2be230732f520616624f1c96
Julia Eva <julia.vazquez289@gmail.com>
2024-01-10 17:43:38 -0300

    Añade .gitignore para excluir node_modules

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+
+//app.get("/products", (req,res)=>{})
+
+//app.get("/products/:pid", (req,res)=>{})
+
+
+app.listen(8080, ()=> (console.log('Aplicación funcionando en el puerto 8080')))
