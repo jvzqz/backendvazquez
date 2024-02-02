b869f83ad0c6419013f4f31e77704fcd54e3ec97
Julia Eva <julia.vazquez289@gmail.com>
2024-02-02 11:43:43 -0300

    websocket

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import express from 'express';
-import cartsrouter from './routes/carts.router.js';
-import productsrouter from './routes/products.router.js';
-import viewsrouter from './routes/views.router.js'
 import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
+import cartsrouter from './routes/carts.router.js';
+import productsrouter from './routes/products.router.js';
+import viewsrouter from './routes/views.router.js';
 import { Server } from 'socket.io';
 
 const app = express();
 const httpServer = app.listen(8080, ()=> 
 console.log('Server running in port 8080')
 );
-const socketServer = new Server(httpServer);
+const io = new Server(httpServer);
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -23,11 +23,11 @@ app.use(express.static(__dirname+'/public'));
 
 app.use('/api/products', productsrouter);
 app.use ('/api/carts', cartsrouter);
-app.use ('/api/viewsrouter', viewsrouter);
+app.use ('/', viewsrouter);
 
 const ListProducts = [];
 io.on('connection', socket => {
-    socket.on('message', data => {
+    socket.on('products', data => {
         ListProducts.push(data)
         io.emit('productLogs', ListProducts)
     })
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
-const router = express.Router();
+const viewsrouter = express.Router();
 
-router.get('/', (req,res)=>{
+viewsrouter.get('/', (req,res)=>{
     res.render('home', { products })
 })
 
-router.post('/realtimeproducts',(req,res)=>{
+viewsrouter.post('/realtimeproducts',(req,res)=>{
     res.render('realTimeProducts',{ products })
 
     const socket = io();
